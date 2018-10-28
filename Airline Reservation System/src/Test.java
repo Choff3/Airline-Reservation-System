adf6f4b2909a8546d8ab4331ff79fe58dcc2aecd
Chastity <Chastity@10.0.0.161>
2018-10-28 13:55:54 -0400

    added second test comment

--- Airline Reservation System/src/Test.java
+++ Airline Reservation System/src/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		//this class is for testing
 		System.out.println("hello");
+		
+		//this is araybia's comment
+		System.out.println("hi, how are you");
 
 	}
 
