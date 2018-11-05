374f36c6d9e39d7f69da546334cedb54d4cb0ade
Charles Hoff <choff3@student.gsu.edu>
2018-11-05 13:06:46 -0500

    Made classes for Users, Customers and Admins

---
+++ Airline Reservation System/src/Customer.java
@@ -0,0 +1,9 @@
+
+public class Customer extends User {
+
+	public Customer(String firstname, String lastname, String address, int zip, String state, String username,
+			String password, String email, String ssn, String question, String answer) {
+		super(firstname, lastname, address, zip, state, username, password, email, ssn, question, answer);
+	}
+
+}
