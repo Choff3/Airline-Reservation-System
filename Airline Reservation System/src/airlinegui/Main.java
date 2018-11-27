cf0b5fafc1844b9e2726065d5e4a047f008a0135
Charles Hoff <choff3@student.gsu.edu>
2018-11-27 17:41:55 -0500

    Created classes and scenes for the gui

---
+++ Airline Reservation System/src/airlinegui/Main.java
@@ -0,0 +1,24 @@
+package airlinegui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public void start(Stage primaryStage) throws Exception{
+	
+	Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+	primaryStage.setTitle("Flight Application");
+	primaryStage.setScene(new Scene(root,1000,1000));
+	primaryStage.show();
+	
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
